fbecc72cc27730b0acf9fa9b26e012ea937f1aa0
Joshua <e0544333@u.nus.edu>
2022-03-24 13:43:12 +0800

    Implement skeletal code for multithreaded process
    {current situation} -- need to satisfy multithreaded instances where different clients will request from the server.
    
    {why it needs to change} - nil
    
    {what is being done about it} -- created the base code for multithreaded process. Moreover, created an index and 404 page to simulate requests.
    
    {why it is done that way} - a Threadpool is created to facilitate the process of workers <-> services.  Things to be done is to document the different sections of code. Especially src/lib.rs.
    
    {any other relevant info}
    Refer to this Rust development book on creating multi-threaded Web Server
    https://doc.rust-lang.org/book/ch20-00-final-project-a-web-server.html

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -7,16 +7,21 @@ use std::net::TcpStream;
 
 use std::thread;
 use std::time::Duration;
+use fast_rpc::ThreadPool;
 
 // Code adapted from https://doc.rust-lang.org/book/ch20-01-single-threaded.html
 fn main() {
     // returns an iterator that gives us a sequence of streams. More specifically,
     //streams of type TcpStream.
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
     // When stream goes out of scope and is dropped at the end of the loop,
     // the connection is closed as part of the drop implementation.
